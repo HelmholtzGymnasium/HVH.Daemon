99477e6f6a0c55a61ed28f30c59a3c8f96704eb0
Dorian Stoll <dorian@tmsp.ga>
2016-12-01 20:08:00 +0100

    Communication and more encryption

--- src/Encryption/NoneEncryptionProvider.cs
+++ src/Encryption/NoneEncryptionProvider.cs
@@ -11,28 +11,18 @@ using HVH.Service.Interfaces;
 namespace HVH.Service.Encryption
 {
     /// <summary>
-    /// Encrypts data using RSA keys - internal use only
+    /// IEncryptionProvider shim for no encryption
     /// </summary>
-    internal class RSAEncryptionProvider : IEncryptionProvider 
+    public class NoneEncryptionProvider  : IEncryptionProvider
     {
-        /// <summary>
-        /// A RSA that is used to encrypt the 
-        /// </summary>
-        public RSACryptoServiceProvider key;
-
-        public RSAEncryptionProvider(Int32 keySize)
-        {
-            key = new RSACryptoServiceProvider(keySize);
-        }
-
         public Byte[] Encrypt(Byte[] data)
         {
-            return key.Encrypt(data, false);
+            return data;
         }
 
         public Byte[] Decrypt(Byte[] data)
         {
-            return key.Decrypt(data, false);
+            return data;
         }
 
         public void ChangeKey(Byte[] newKey)
